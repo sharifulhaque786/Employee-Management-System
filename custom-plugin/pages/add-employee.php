f6546c4799c4fdd87f18778ac921d7bc740be27a
Md Shariful Haque <sharifulhaque786@gmail.com>
2024-02-23 12:38:44 +0400

    add employee update

--- custom-plugin/pages/add-employee.php
+++ custom-plugin/pages/add-employee.php
@@ -18,18 +18,33 @@
             <div class="panel-body">
                 <form action="/action_page.php">
                     <div class="mb-3 mt-3">
+                        <label for="name" class="form-label">Name:</label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+                    </div>
+                    <div class="mb-3 mt-3">
                         <label for="email" class="form-label">Email:</label>
                         <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                     </div>
+                    <div class="mb-3 mt-3">
+                        <label for="phone" class="form-label">Phone:</label>
+                        <input type="phone" class="form-control" id="phone" placeholder="Enter phone" name="phone">
+                    </div>
                     <div class="mb-3">
-                        <label for="pwd" class="form-label">Password:</label>
-                        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                        <label for="gender" class="form-label">Gender:</label>
+                        <select id="gender" name="gender" class="form-control">
+                            <option>Select Option</option>
+                            <option>Male</option>
+                            <option>Female</option>
+                            <option>Other</option>
+                        </select>
                     </div>
-                    <div class="form-check mb-3">
-                        <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" name="remember"> Remember me
-                        </label>
+
+                    <div class="mb-3 mt-3">
+                        <label for="designation" class="form-label">Designation:</label>
+                        <input type="designation" class="form-control" id="designation" placeholder="Enter designation"
+                            name="designation">
                     </div>
+
                     <button type="submit" class="btn btn-success">Submit</button>
                 </form>
             </div>
