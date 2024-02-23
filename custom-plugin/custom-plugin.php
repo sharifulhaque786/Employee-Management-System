b88211909e0b21f5e712495b0d54603f3145b92e
Md Shariful Haque <sharifulhaque786@gmail.com>
2024-02-23 12:59:34 +0400

    another update

--- custom-plugin/custom-plugin.php
+++ custom-plugin/custom-plugin.php
@@ -1,12 +1,12 @@
 <?php
 /*
-* Plugin Name:   Employee Management System
-* Description:   Our custom WordPress plugin enhances your website's functionality by adding tailored features designed specifically to meet your needs.
-* Plugin URI:   https://sfl.media/
-* Author:   Shariful Haque
-* Author URI:   https://sfl.media/
-* Version:   1.0.0
-*/
+ * Plugin Name:   Employee Management System
+ * Description:   Our custom WordPress plugin enhances your website's functionality by adding tailored features designed specifically to meet your needs.
+ * Plugin URI:   https://sfl.media/
+ * Author:   Shariful Haque
+ * Author URI:   https://sfl.media/
+ * Version:   1.0.0
+ */
 
 define("EMS_PLUGIN_PATH", plugin_dir_path(__FILE__));
 
@@ -16,9 +16,9 @@ function ems_add_admin_menu()
 {
     add_menu_page("Employee System | Employee Management System", "Employee System", "manage_options", "employee-system", "ems_crud_system", "dashicons-admin-home", 23);
 
-    add_submenu_page('employee-system', 'Add Employee', 'Add Employee', "manage_options", 'employee-system', 'ems_crud_system',);
+    add_submenu_page('employee-system', 'Add Employee', 'Add Employee', "manage_options", 'employee-system', 'ems_crud_system', );
 
-    add_submenu_page('employee-system', 'List Employee', 'List Employee', "manage_options", 'list-employee', 'ems_list_employee',);
+    add_submenu_page('employee-system', 'List Employee', 'List Employee', "manage_options", 'list-employee', 'ems_list_employee', );
 }
 
 function ems_crud_system()
--- custom-plugin/pages/add-employee.php
+++ custom-plugin/pages/add-employee.php
@@ -13,40 +13,46 @@
 <body>
 
     <div class="container">
-        <h2>Add Employee</h2>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <form action="/action_page.php">
-                    <div class="mb-3 mt-3">
-                        <label for="name" class="form-label">Name:</label>
-                        <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
-                    </div>
-                    <div class="mb-3 mt-3">
-                        <label for="email" class="form-label">Email:</label>
-                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-                    </div>
-                    <div class="mb-3 mt-3">
-                        <label for="phone" class="form-label">Phone:</label>
-                        <input type="phone" class="form-control" id="phone" placeholder="Enter phone" name="phone">
-                    </div>
-                    <div class="mb-3">
-                        <label for="gender" class="form-label">Gender:</label>
-                        <select id="gender" name="gender" class="form-control">
-                            <option>Select Option</option>
-                            <option>Male</option>
-                            <option>Female</option>
-                            <option>Other</option>
-                        </select>
-                    </div>
+        <div class="row">
+            <div class="class-sm-8">
+                <h2>Add Employee</h2>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <form action="/action_page.php">
+                            <div class="mb-3 mt-3">
+                                <label for="name" class="form-label">Name:</label>
+                                <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+                            </div>
+                            <div class="mb-3 mt-3">
+                                <label for="email" class="form-label">Email:</label>
+                                <input type="email" class="form-control" id="email" placeholder="Enter email"
+                                    name="email">
+                            </div>
+                            <div class="mb-3 mt-3">
+                                <label for="phone" class="form-label">Phone:</label>
+                                <input type="phone" class="form-control" id="phone" placeholder="Enter phone"
+                                    name="phone">
+                            </div>
+                            <div class="mb-3" mt-3>
+                                <label for="gender" class="form-label">Gender:</label>
+                                <select id="gender" name="gender" class="form-control">
+                                    <option>Select Option</option>
+                                    <option>Male</option>
+                                    <option>Female</option>
+                                    <option>Other</option>
+                                </select>
+                            </div>
 
-                    <div class="mb-3 mt-3">
-                        <label for="designation" class="form-label">Designation:</label>
-                        <input type="designation" class="form-control" id="designation" placeholder="Enter designation"
-                            name="designation">
-                    </div>
+                            <div class="mb-3 mt-3">
+                                <label for="designation" class="form-label">Designation:</label>
+                                <input type="designation" class="form-control" id="designation"
+                                    placeholder="Enter designation" name="designation">
+                            </div>
 
-                    <button type="submit" class="btn btn-success">Submit</button>
-                </form>
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
